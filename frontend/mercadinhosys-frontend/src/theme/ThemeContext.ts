2cee658d3049eb4364b8950dc1a9bcbff3cad95c
RafaelMaldivas <rafaelmaldivas@gmail.com>
2026-01-02 04:25:07 -0400

    fix ThemeContext

--- frontend/mercadinhosys-frontend/src/theme/ThemeContext.ts
+++ frontend/mercadinhosys-frontend/src/theme/ThemeContext.ts
@@ -6,4 +6,7 @@ export interface ThemeContextType {
     toggleTheme: () => void;
 }
 
-export const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+export const ThemeContext = createContext<ThemeContextType>({
+    mode: 'dark',
+    toggleTheme: () => {},
+});
