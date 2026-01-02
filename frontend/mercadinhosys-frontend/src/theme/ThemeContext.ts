08d8e6855db8b88d6072d5cf8eb6c7cd4cd3dbff
RafaelMaldivas <rafaelmaldivas@gmail.com>
2026-01-02 04:21:11 -0400

    fix: corrige erros de build TypeScript

---
+++ frontend/mercadinhosys-frontend/src/theme/ThemeContext.ts
@@ -0,0 +1,9 @@
+// src/theme/ThemeContext.ts
+import { createContext } from 'react';
+
+export interface ThemeContextType {
+    mode: 'dark' | 'light';
+    toggleTheme: () => void;
+}
+
+export const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
