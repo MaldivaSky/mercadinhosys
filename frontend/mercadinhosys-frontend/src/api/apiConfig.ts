16fd8b554cbf33cbb027cc43e3325e24f76b9817
RafaelMaldivas <rafaelmaldivas@gmail.com>
2026-01-06 08:15:09 -0400

    alterando API para render, vercel

--- frontend/mercadinhosys-frontend/src/api/apiConfig.ts
+++ frontend/mercadinhosys-frontend/src/api/apiConfig.ts
@@ -1,5 +1,17 @@
 // src/api/apiConfig.ts
+
+// Esta função decide qual link usar automaticamente
+const getBaseUrl = () => {
+    // Se estiver rodando no seu computador (npm run dev)
+    if (import.meta.env.MODE === 'development') {
+        return 'http://localhost:5000/api';
+    }
+
+    // Se estiver rodando na Vercel ou produção (npm run build)
+    return 'https://mercadinhosys.onrender.com/api';
+};
+
 export const API_CONFIG = {
-    BASE_URL: 'http://localhost:5000/api',
-    TIMEOUT: 10000,
+    BASE_URL: getBaseUrl(),
+    TIMEOUT: 20000, // Aumentei um pouco para garantir no 3G/4G
 } as const;
