76d16bc2a88ec171c8cab985f8ade168668eba62
RafaelMaldivas <rafaelmaldivas@gmail.com>
2026-01-20 23:09:22 -0400

    correções no backend

--- backend/app/utils/__init__.py
+++ backend/app/utils/__init__.py
@@ -1,5 +1,5 @@
 # Importa as funções do arquivo utils.py para facilitar o acesso
-from .utils import calcular_margem_lucro, validar_cnpj, validar_cpf, formatar_moeda, formatar_codigo_barras, validar_email, formatar_telefone
+from .utils import calcular_margem_lucro, validar_cnpj, validar_cpf, formatar_moeda, formatar_codigo_barras, validar_email, formatar_telefone, calcular_idade
 
 # Importa o logger
 from .logger import setup_logger
