f3648a1db75bd2c991f94b7557742cabe432c023
RafaelMaldivas <rafaelmaldivas@gmail.com>
2026-01-30 01:18:50 -0400

    feat: ativa HeaderProfessional no MainLayout

--- frontend/mercadinhosys-frontend/src/components/layout/MainLayout.tsx
+++ frontend/mercadinhosys-frontend/src/components/layout/MainLayout.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
-import Header from './Header';
+import HeaderProfessional from './HeaderProfessional';
 import FootPage from './FootPage';
 
 const MainLayout: React.FC = () => {
@@ -10,7 +10,7 @@ const MainLayout: React.FC = () => {
         <div className="flex h-screen bg-gray-50 dark:bg-gray-900">
             <Sidebar />
             <div className="flex-1 flex flex-col overflow-hidden">
-                <Header />
+                <HeaderProfessional />
                 <main className="flex-1 overflow-y-auto p-4 md:p-6">
                     {/* O Outlet renderiza as páginas aninhadas */}
                     <Outlet />
