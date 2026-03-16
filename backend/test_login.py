78da6c2cfebb6d5706185591f5b6445cb5f1d8f8
RafaelMaldivas <rafaelmaldivas@gmail.com>
2026-03-15 23:37:31 -0400

    CLEAN: Limpeza de arquivos de debug e atualização do .gitignore

--- backend/test_login.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = 'http://localhost:5000/api/auth/login'
-headers = {'Content-Type': 'application/json'}
-data = {'username': 'maldivas', 'senha': 'Mald1v@$'}
-
-response = requests.post(url, json=data, headers=headers)
-print('Status Code:', response.status_code)
-print('Response JSON:', response.json())
